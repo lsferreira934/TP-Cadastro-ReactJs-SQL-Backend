3f9e792b1a1213dab8a7beb1243b2b122bcae308
Leandro <leandrosf934@gmail.com>
2020-11-05 16:33:03 -0300

    mudança procfile

--- src/config/config.js
+++ src/config/config.js
@@ -35,10 +35,7 @@ module.exports = {
     database: {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
-      name: process.env.DB_NAME,
       dialect: 'mysql',
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
     },
   },
 };
