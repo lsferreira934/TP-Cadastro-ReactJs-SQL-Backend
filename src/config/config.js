99bb3831e5c91bd01623e7c0a82f48e3ce0043eb
Diego <dieguinhoandrade@hotmail.com>
2020-11-05 14:55:21 -0300

    v1.0

--- src/config/config.js
+++ src/config/config.js
@@ -22,12 +22,12 @@
 module.exports = {
   development: {
     database: {
-      host: 'localhost',
-      port: '3306',
-      name: 'db_sistema_venda',
-      dialect: 'mysql',
-      user: 'root',
-      password: 'root',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      name: process.env.DB_NAME,
+      dialect: process.env.DB_DIALECT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
     },
   },
   production: {
